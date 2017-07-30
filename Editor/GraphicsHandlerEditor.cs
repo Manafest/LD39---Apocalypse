510c842e5d6817104ce9986e5dad5d8b7b46ddb7
Manafest <2590499+Manafest@users.noreply.github.com>
2017-07-31 00:31:28 +0100

    Add files via upload

---
+++ Editor/GraphicsHandlerEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GraphicsHandler))]
+public class GraphicsHandlerEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		GraphicsHandler myScript = (GraphicsHandler)target;
+		DrawDefaultInspector ();
+
+		if (GUILayout.Button ("Initalise")) 
+		{
+			myScript.CreateLevel();
+		}
+
+	}
+}
